e771699bf28ec16f17cb775447e96340258fab6b
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-12-02 18:06:47 +0900

    Add error page

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,9 @@
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
+      <div class="links">
+        <router-link to="pageHasError">PageHasError</router-link>
+      </div>
     </div>
   </section>
 </template>
---
+++ pages/pageHasError.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Page has error</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    // エラー
+    console.log(this.user.name)
+    return {}
+  }
+}
+</script>
